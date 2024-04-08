e263dce4273acc73d03ebc95e78d7dfc09081a9d
pw <poomrapee.war@student.mahidol.edu>
2024-04-08 13:52:31 +0700

    adjust recipe replacement approach

--- adjust_recipe/replace.py
+++ adjust_recipe/replace.py
@@ -1,6 +1,22 @@
 import json
+import re
+
 from pathlib import Path
 from tqdm import tqdm
+
+def replace_words(text, old_word, new_word):
+    # This pattern ensures that the word boundaries are respected
+    # so only whole words are replaced, and not parts of other words.
+    # \b is a word boundary, \W is any non-word character.
+    pattern = r'(\b){}(\b)|(\W)'.format(re.escape(old_word))
+    
+    def replace(match):
+        if match.group(0) == old_word:
+            return new_word
+        else:
+            return match.group(0)  # Return non-word characters as is
+    
+    return re.sub(pattern, replace, text)
 
 def main():
     substitute_pairs_file_path = 'adjust_recipe/data/food2vec_5nn_result_1.json'
@@ -37,7 +53,7 @@ def main():
                 target = list(substitution.keys())[0]
                 replacement = f'{target} ({' / '.join(substitution[target])})'
 
-                recipe['instructions'][i]['text'] = instruction['text'].replace(target, replacement)
+                recipe['instructions'][i]['text'] = replace_words(instruction['text'], target, replacement)
         
     with Path(new_recipes_export_path).open('w') as f:
         json.dump(recipes, f, indent=2)
